4c7961329c925390e1c93e16bd7cdb54a790f69a
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-22 18:09:26 +0100

    Hecho npx + incorporado HTML contador inicial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+  let valorActual=0
+
+  return (
+    <div>
+      <h1>{valorActual}</h1>
+      <button>Contador</button>
+    </div>
+  );
+}
+
+export default App;
