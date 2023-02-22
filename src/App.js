3c6a85dcf3a6ed68fbb359ded921b41896eeaf75
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-22 19:44:21 +0100

    Inclusión botones + - y reset

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
+import { useState } from "react";
 
 function App() {
 
-  let valorActual=0
+  let [valorActual, setvalorActual]= useState (0)
+
+  function manexadorClickAumenta () {
+    let novoValor = valorActual+1
+    setvalorActual(novoValor)
+  }
+
+  function manexadorClickDisminuye () {
+    let novoValor = valorActual-1
+    setvalorActual(novoValor)
+  }
+
+  function manexadorClickReset () {
+    let novoValor = 0
+    setvalorActual(novoValor)
+  }
 
   return (
     <div>
       <h1>{valorActual}</h1>
-      <button>Contador</button>
+      <button onClick={manexadorClickAumenta}>Aumentador</button>
+      <button onClick={manexadorClickDisminuye}>Disminuidor</button>
+      <button onClick={manexadorClickReset}>Reset</button>
     </div>
   );
 }
