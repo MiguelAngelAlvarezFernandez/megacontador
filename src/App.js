98c5fc77170a5799359e458bb28736d293dede4e
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-22 19:46:31 +0100

    Eliminado botón reset

--- src/App.js
+++ src/App.js
@@ -14,17 +14,12 @@ function App() {
     setvalorActual(novoValor)
   }
 
-  function manexadorClickReset () {
-    let novoValor = 0
-    setvalorActual(novoValor)
-  }
 
   return (
     <div>
       <h1>{valorActual}</h1>
       <button onClick={manexadorClickAumenta}>Aumentador</button>
       <button onClick={manexadorClickDisminuye}>Disminuidor</button>
-      <button onClick={manexadorClickReset}>Reset</button>
     </div>
   );
 }
