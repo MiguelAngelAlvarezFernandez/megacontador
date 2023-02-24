0d4c64f16e2a82de3d8deef266fed3837f3da3fe
MiguelAngelAlvarezFernandez <mayprofprog@gmail.com>
2023-02-24 20:54:04 +0100

    Añadido botón Cronómetro con función inicio y stop

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Boton from "./Components/Boton";
 
 function App() {
 
   let [valorActual, setvalorActual]= useState (0)
+  let [Cronometro, setCronometro] = useState (false)
 
   function Aumentar () {
     let novoValor = valorActual+1
@@ -20,12 +21,31 @@ function App() {
     setvalorActual(novoValor)
   }
 
+  function ActivarCronometro () {
+    setCronometro(!Cronometro) /*Así niego cronómetro, de forma
+                               si empieza siendo true pasa a false
+                               y viceversa lo que me permitirá tener
+                               en un solo botón el inicio y stop del
+                               Cronometro */ 
+  }
+
+  useEffect (
+  () => { 
+    let CodCronometro
+    if (Cronometro) {CodCronometro = setTimeout(Aumentar, 1000)}
+    return ()=>{clearTimeout(CodCronometro)}
+  }, 
+  [Cronometro, valorActual] ) 
+  
+
+
   return (
     <div>
       <h1>{valorActual}</h1>
       <Boton NombreBoton="+" YDespuesDeContarQue={Aumentar} Contador/>
       <Boton NombreBoton="-" YDespuesDeContarQue={Disminuir} Contador/>
       <Boton NombreBoton="Reset" YDespuesDeContarQue={Resetear}/>
+      <Boton NombreBoton="Cronometro" YDespuesDeContarQue={ActivarCronometro}/>
     </div>
   );
 }
